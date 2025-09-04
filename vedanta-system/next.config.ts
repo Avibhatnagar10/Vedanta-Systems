b1afaefe0facf8a32a57ee8db726da51df52e769
Avi Bhatnagar <91671078+Avibhatnagar10@users.noreply.github.com>
2025-09-04 13:06:35 +0530

    Update next.config.ts

--- vedanta-system/next.config.ts
+++ vedanta-system/next.config.ts
@@ -1,7 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["unsplash.com", "images.unsplash.com"], // whitelist Unsplash domains
+    domains: ["unsplash.com", "images.unsplash.com","lh3.googleusercontent.com"],
+     remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '**',
+      },
+    ], // whitelist Unsplash domains
   },
 };
 
